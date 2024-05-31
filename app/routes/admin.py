43e869e1cb14b259136ddde66c529648ab01cb88
Nadzimushsyahar Bin Mohd Halel <35636377+Greenbonee@users.noreply.github.com>
2024-05-31 17:24:18 +0800

    Delete app directory

--- app/routes/admin.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('admin', __name__, url_prefix='/admin')
-
-@bp.route('/backup', methods=['GET', 'POST'])
-def backup():
-    # Backup logic here
-    return render_template('admin/backup.html')
--- app/routes/customer.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('customer', __name__, url_prefix='/customer')
-
-@bp.route('/register', methods=['GET', 'POST'])
-def register():
-    # Registration logic here
-    return render_template('customer/register.html')
